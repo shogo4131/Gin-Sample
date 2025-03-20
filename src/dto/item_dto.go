91eccda5dc71255432adb7ac371a292272e32cd9
shogo4131 <shougo08697118@gmail.com>
2025-03-20 20:18:55 +0900

    feat: model & dtoの追加

---
+++ src/dto/item_dto.go
@@ -0,0 +1,14 @@
+package dto
+
+type CreateItemInput struct {
+	Name        string `json:"name" binding:"required,min=2"`
+	Price       uint   `json:"price" binding:"required,min=1,max=999999"`
+	Description string `json:"description" binding:"required,min=2"`
+}
+
+type UpdateItemInput struct {
+	Name        *string `json:"name" binding:"required,min=2"`
+	Price       *uint   `json:"price" binding:"required,min=1,max=999999"`
+	Description *string `json:"description" binding:"required,min=2"`
+	SoldOut     *bool   `json:"soldOut"`
+}
