01b57ad5c98122c63c772960e4f02c7b06eef92b
shogo4131 <shougo08697118@gmail.com>
2025-03-26 10:29:09 +0900

    feat: corsと認証チェックを追加

--- apps/cmd/main.go
+++ apps/cmd/main.go
@@ -4,9 +4,11 @@ import (
 	"go-gin-sample/apps/config"
 	"go-gin-sample/apps/controllers"
 	"go-gin-sample/apps/db"
+	"go-gin-sample/apps/middleware"
 	"go-gin-sample/apps/repository"
 	"go-gin-sample/apps/services"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -24,14 +26,17 @@ func main() {
 	authController := controllers.NewAuthController(authService)
 
 	r := gin.Default()
+	r.Use(cors.Default())
+
 	itemRouter := r.Group("/items")
+	itemRouterWithAuth := r.Group("/items", middleware.AuthMiddleware((authService)))
 	authRouter := r.Group("/auth")
 
 	itemRouter.GET("", itemController.FindAll)
-	itemRouter.GET("/:id", itemController.FindById)
-	itemRouter.POST("", itemController.Create)
-	itemRouter.PUT("/:id", itemController.Update)
-	itemRouter.DELETE("/:id", itemController.Delete)
+	itemRouterWithAuth.GET("/:id", itemController.FindById)
+	itemRouterWithAuth.POST("", itemController.Create)
+	itemRouterWithAuth.PUT("/:id", itemController.Update)
+	itemRouterWithAuth.DELETE("/:id", itemController.Delete)
 
 	authRouter.POST("/signup", authController.Signup)
 	authRouter.POST("/login", authController.Login)
